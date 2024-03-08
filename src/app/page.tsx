3311cd9fe159033b05c0ceda6096fd913ca5d0cb
richfjiq <rfjiq1986@gmail.com>
2024-03-08 07:04:03 -0600

    chore: adds form to create topics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
-import * as actions from '@/actions';
-import { auth } from '@/auth';
+import TopicCreateForm from '@/components/topics/topic-create-form';
 
 export default async function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }

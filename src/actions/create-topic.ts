f3164600b423068bd3d5b8cabd952a747ef4fa4f
richfjiq <rfjiq1986@gmail.com>
2024-03-09 16:12:12 -0600

    chore: adds handling database errors in forms

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,13 @@
 'use server';
 
 import { z } from 'zod';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+import { auth } from '@/auth';
+import type { Topic } from '@prisma/client';
+import { db } from '@/db';
+import paths from '@/paths';
 
 const createTopicSchema = z.object({
   name: z
@@ -16,6 +23,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 
@@ -34,8 +42,40 @@ export async function createTopic(
     };
   }
 
-  return {
-    errors: {},
-  };
-  // TODO: revalidate the home page
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this.'],
+      },
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
+
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
 }

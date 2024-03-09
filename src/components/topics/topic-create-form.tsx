56caa7f8d6aee7b2b1676bbb6dfba7a1149bc6fb
richfjiq <rfjiq1986@gmail.com>
2024-03-09 16:44:22 -0600

    chore: fetching and lsiting content

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -10,6 +10,7 @@ import {
 } from '@nextui-org/react';
 
 import * as actions from '@/actions';
+import FormButton from '@/components/common/form-button';
 
 export default function TopicCreateForm() {
   const [formState, action] = useFormState(actions.createTopic, {
@@ -47,7 +48,7 @@ export default function TopicCreateForm() {
                 {formState.errors._form?.join(', ')}
               </div>
             ) : null}
-            <Button type="submit">Submit</Button>
+            <FormButton>Save</FormButton>
           </div>
         </form>
       </PopoverContent>

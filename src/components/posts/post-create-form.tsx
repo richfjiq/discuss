aa0e2c22ab6f2b0a46a9b6ca46a36cfeace62f09
richfjiq <rfjiq1986@gmail.com>
2024-03-10 20:00:53 -0600

    chore: adds validation to post creation

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -13,22 +13,30 @@ import * as actions from '@/actions';
 import FormButton from '@/components/common/form-button';
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Post</h3>
             <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
               name="title"
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
             />
             <Input
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
               name="content"
               label="Content"
               labelPlacement="outside"

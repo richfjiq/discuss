d48e4ae59c435ac9ed8bf039bb84981e508fafa5
richfjiq <rfjiq1986@gmail.com>
2024-03-14 20:54:30 -0600

    chore; adds fetching comments list

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,7 @@ import CommentCreateForm from '@/components/comments/comment-create-form';
 import CommentList from '@/components/comments/comment-list';
 import paths from '@/paths';
 import PostShow from '@/components/posts/post-show';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
 
 interface PostShowPageProps {
   params: {
@@ -22,7 +23,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       </Link>
       <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
-      {/* <CommentList /> */}
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
     </div>
   );
 }

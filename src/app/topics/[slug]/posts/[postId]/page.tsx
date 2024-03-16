b2322a4fb5fcc8630d206f3d6b71477c854a1c4b
richfjiq <rfjiq1986@gmail.com>
2024-03-15 22:39:05 -0600

    chore: adds a loading skeleton

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 import CommentCreateForm from '@/components/comments/comment-create-form';
 import CommentList from '@/components/comments/comment-list';
 import paths from '@/paths';
 import PostShow from '@/components/posts/post-show';
 import { fetchCommentsByPostId } from '@/db/queries/comments';
+import PostShowLoading from '@/components/posts/post-show-loading';
 
 interface PostShowPageProps {
   params: {
@@ -21,7 +23,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {'< '}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       {/* <CommentList fetchData={() => fetchCommentsByPostId(postId)} /> */}
       <CommentList postId={postId} />
